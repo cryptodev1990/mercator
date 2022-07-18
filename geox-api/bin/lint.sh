9e5b17123931214b35220d64c4d765302359ca8e
cryptodev1990 <raylane19990105@gmail.com>
2022-07-18 09:28:10 -0700

    🎨  Add yaml linter

--- geox-api/bin/lint.sh
+++ geox-api/bin/lint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Lints all files in the project
 set -x
+# do not set -e because we want to run all the commands
 
 # Lint python files
 _lint_python() {
@@ -15,6 +16,12 @@ _lint_shell() {
     shellcheck -e SC1090 -S warning -- *.sh bin/*.sh
 }
 
+# Lint yaml with https://github.com/adrienverge/yamllint
+_lint_yaml() {
+    yamllint ./*.yml
+}
+
 # TODO: could customize which are run
 _lint_python
 _lint_shell
+_lint_yaml
