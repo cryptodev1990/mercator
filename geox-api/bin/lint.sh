f6b6cefe04f36df47ccb5f637cc03eaba6ff8782
cryptodev1990 <raylane19990105@gmail.com>
2022-07-18 13:22:19 -0700

    🐛  Lint script exits with non-zero if any fail

--- geox-api/bin/lint.sh
+++ geox-api/bin/lint.sh
@@ -3,26 +3,49 @@
 set -x
 # do not set -e because we want to run all the commands
 
+function check_status {
+    "$@"
+    local status=$?
+    if (( status != 0 )); then
+        echo "error with $1" >&2
+    fi
+    return $status
+}
+
 # Lint python files
-_lint_python() {
+function _mypy() {
     mypy ./app
+}
+
+function _black() {
     black ./app --check
+}
+
+function _isort() {
     isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only ./app ./alembic/env.py
 }
 
 # Lint shell scripts (*.sh)
-_lint_shell() {
+function _shellcheck() {
     shellcheck -e SC1090 -S warning -- *.sh bin/*.sh
 }
 
 # Lint yaml with https://github.com/adrienverge/yamllint
-_lint_yaml() {
+function _yamllint() {
     # Relaxed is less strict about some things
     # See https://yamllint.readthedocs.io/en/stable/configuration.html#default-configuration
     yamllint -c yamllint.yml ./*.yml
 }
 
 # TODO: could customize which are run
-_lint_python
-_lint_shell
-_lint_yaml
+
+cmds="_mypy _black _isort _shellcheck _yamllint"
+num_failures=0
+for cmd in $cmds;
+do
+    check_status $cmd
+    # increment if failed
+    let num_failures+=$?
+done
+# Exit code is the number of failed tests
+exit $num_failures
