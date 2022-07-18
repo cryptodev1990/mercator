ca3b900cd61492f926dad4570a59749741ace804
cryptodev1990 <raylane19990105@gmail.com>
2022-07-18 09:42:03 -0700

    :bug: Wrong logic used for checking file existence

--- geox-api/bin/run-with-env.sh
+++ geox-api/bin/run-with-env.sh
@@ -9,7 +9,7 @@
 # ENV_FILE=.env.docker run-with-env env
 set -e
 export ENV_FILE=${ENV_FILE:-.env}
-if [ ! -f "$ENV_FILE" ]; then
+if [ -f "$ENV_FILE" ]; then
     export $(grep -v '^#' $ENV_FILE | xargs)
 fi
 exec "$@"
