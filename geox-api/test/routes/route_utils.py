73b25a6da76d2755e6af4c9935eaea53eaeb48e7
cryptodev1990 <raylane19990105@gmail.com>
2022-09-23 21:56:06 +0200

    [Housekeeping] Move test utilities for shapes into their own module (#279)

---
+++ geox-api/test/routes/route_utils.py
@@ -0,0 +1,102 @@
+from functools import partial
+from typing import Any, Dict
+
+import pytest
+from fastapi import Depends
+from pydantic import UUID4
+from sqlalchemy import delete, insert, select, text, update
+from sqlalchemy.engine import Connection
+from sqlalchemy.orm import Session
+from app.models import Shape
+
+from app import models, schemas
+from app.db.base import Organization, OrganizationMember, User
+from app.db.session import SessionLocal, engine
+from app.dependencies import get_current_user, get_db, verify_token
+from app.main import app
+
+
+def set_active_organization(
+    conn: Connection, user_id: int, organization_id: UUID4
+) -> None:
+    stmt = (
+        update(OrganizationMember)  # type: ignore
+        .where(OrganizationMember.user_id == user_id)
+        .values(active=(OrganizationMember.organization_id == organization_id))
+    )
+    return conn.execute(stmt)
+
+
+@pytest.fixture()
+def connection(test_data: Dict[str, Any]):
+    with engine.connect() as conn:
+        with conn.begin():
+            conn.execute(insert(User), test_data["users"])  # type: ignore
+            conn.execute(
+                insert(Organization), test_data["organizations"]
+            )  # type: ignore
+            for org_member in test_data["organization_members"]:
+                conn.execute(insert(OrganizationMember),
+                             org_member)  # type: ignore
+                # Set these new organizations to the active organization
+                set_active_organization(
+                    conn, org_member["user_id"], org_member["organization_id"]
+                )
+            conn.execute(insert(Shape), test_data["shapes"])  # type: ignore
+            conn.commit()
+
+        yield conn
+
+        # Can't trust routes to handle their transactions correctly so close any open
+        # transactions
+        try:
+            conn.commit()
+        except:
+            pass
+
+        with conn.begin():
+            for tbl in (Shape, OrganizationMember, Organization, User):
+                conn.execute(delete(tbl))  # type: ignore
+            conn.commit()
+
+
+def get_current_user_override(*, user_id: int, db_session: Session = Depends(get_db)):
+    """Return a particular existing user by id.
+
+    This skips authentication of users to allow fake users.
+    """
+    user = db_session.execute(
+        select(models.User).filter(models.User.id == user_id)  # type: ignore
+    ).fetchone()
+    return schemas.User.from_orm(user[0])
+
+
+def get_db_override(connection):
+    def f():
+        db = SessionLocal(bind=connection)
+        try:
+            yield db
+            try:
+                db.commit()
+            except:
+                pass
+        finally:
+            db.close()
+
+    return f
+
+
+@pytest.fixture()
+def dep_override_factory(fastapi_dep, connection):
+    """Return the common dependency overrides."""
+
+    def overrides(user_id: int):
+        return fastapi_dep(app).override(
+            {
+                get_db: get_db_override(connection),
+                get_current_user: partial(get_current_user_override, user_id=user_id),
+                verify_token: lambda: {},
+            }
+        )
+
+    return overrides
--- geox-api/test/routes/test_shapes.py
+++ geox-api/test/routes/test_shapes.py
@@ -1,31 +1,36 @@
 import datetime
 import json
 import pathlib
-from functools import partial
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import List, Optional
 from uuid import UUID
+from pydantic import UUID4
 
 import pytest
 from fastapi import Depends, status
 from fastapi.testclient import TestClient
 from geojson_pydantic import Feature, Point
-from pytest_fastapi_deps import fastapi_dep  # type: ignore
 from ruamel.yaml import YAML
-from sqlalchemy import MetaData, delete, insert, select, text, update
+from sqlalchemy import MetaData, select, text, update
 from sqlalchemy.engine import Connection, Row  # type: ignore
-from sqlalchemy.orm import Session
 
-from app import models, schemas
 from app.core.access_token import get_access_token
 from app.core.config import get_settings
-from app.db.base import Organization, OrganizationMember, User
-from app.db.session import SessionLocal, engine
-from app.dependencies import get_current_user, get_db, verify_token
+from app.db.base import Organization, OrganizationMember
+from app.db.session import engine
 from app.main import app
-from app.models import Organization, OrganizationMember, Shape, User
+from app.models import Organization, OrganizationMember, Shape
 from app.schemas import GeoShapeCreate
-from app.schemas.shape import ShapeCountResponse
+from test.routes.route_utils import (
+    connection,
+    dep_override_factory,
+)
+
+
+def shape_exists(conn: Connection, shape_id: UUID4) -> bool:
+    stmt = select(Shape).filter(Shape.uuid == shape_id)  # type: ignore
+    return bool(conn.execute(stmt).fetchone())
+
 
 yaml = YAML(typ="safe")
 
@@ -101,97 +106,15 @@ def get_user_orgs(conn: Connection, user_id: int) -> List[Row]:
     return conn.execute(stmt).fetchall()
 
 
-def get_user_personal_org(conn: Connection, user_id: int) -> Optional[UUID]:
+def get_user_personal_org(conn: Connection, user_id: int) -> Optional[UUID4]:
     stmt = (
-        select(Organization.id)  # type: ignore
-        .join(OrganizationMember)  # type: ignore
-        .filter(OrganizationMember.user_id == user_id, Organization.is_personal)
+        select(Organization)
+        .with_only_columns([Organization.id])
+        .where(Organization.is_personal)
         .limit(1)
+        .join(OrganizationMember, OrganizationMember.organization_id == Organization.id)
     )
     return conn.execute(stmt).scalar()
-
-
-@pytest.fixture()
-def connection(test_data: Dict[str, Any]):
-
-    with engine.connect() as conn:
-        with conn.begin():
-            conn.execute(insert(User), test_data["users"])  # type: ignore
-            conn.execute(
-                insert(Organization), test_data["organizations"]
-            )  # type: ignore
-            for org_member in test_data["organization_members"]:
-                conn.execute(insert(OrganizationMember),
-                             org_member)  # type: ignore
-                # Set these new organizations to the active organization
-                set_active_organization(
-                    conn, org_member["user_id"], org_member["organization_id"]
-                )
-            conn.execute(insert(Shape), test_data["shapes"])  # type: ignore
-            conn.commit()
-
-        yield conn
-
-        # Can't trust routes to handle their transactions correctly so close any open
-        # transactions
-        try:
-            conn.commit()
-        except:
-            pass
-
-        with conn.begin():
-            for tbl in (Shape, OrganizationMember, Organization, User):
-                conn.execute(delete(tbl))  # type: ignore
-            conn.commit()
-
-        # clear_all_tables(conn, Base.metadata(bind=conn))
-
-
-def get_current_user_override(*, user_id: int, db_session: Session = Depends(get_db)):
-    """Return a particular existing user by id.
-
-    This skips authentication of users to allow fake users.
-    """
-    user = db_session.execute(
-        select(models.User).filter(models.User.id == user_id)  # type: ignore
-    ).fetchone()
-    return schemas.User.from_orm(user[0])
-
-
-def get_db_override(connection):
-    def f():
-        db = SessionLocal(bind=connection)
-        try:
-            yield db
-            try:
-                db.commit()
-            except:
-                pass
-        finally:
-            db.close()
-
-    return f
-
-
-@pytest.fixture()
-def dep_override_factory(fastapi_dep, connection):
-    """Return the common dependency overrides."""
-
-    def overrides(user_id: int):
-        return fastapi_dep(app).override(
-            {
-                get_db: get_db_override(connection),
-                get_current_user: partial(get_current_user_override, user_id=user_id),
-                verify_token: lambda: {},
-            }
-        )
-
-    return overrides
-
-
-def shape_exists(conn: Connection, shape_id: UUID) -> bool:
-    stmt = select(Shape).filter(Shape.uuid == shape_id)  # type: ignore
-    return bool(conn.execute(stmt).fetchone())
 
 
 def test_policy_exists(connection):
@@ -206,21 +129,22 @@ def test_policy_exists(connection):
 
 def test_read_shape_self(connection, dep_override_factory):
     user_id = 1
-    shape_id = "4f974f2d-572b-46f1-8741-56bf7f357d12"
+    shape_id = UUID("4f974f2d-572b-46f1-8741-56bf7f357d12")
     assert shape_exists(connection, shape_id)
     with dep_override_factory(user_id):
         response = client.get(f"/geofencer/shapes/{shape_id}")
         body = response.json()
         assert body
-        assert body.get("uuid") == shape_id
+        assert UUID(body.get("uuid")) == shape_id
 
 
 def test_read_shape_self_wrong_org(connection, dep_override_factory):
 
     user_id = 1
-    shape_id = "4f974f2d-572b-46f1-8741-56bf7f357d12"
+    shape_id = UUID("4f974f2d-572b-46f1-8741-56bf7f357d12")
     # Change the org to the user's personal org
     personal_org = get_user_personal_org(connection, user_id)
+    assert personal_org
     set_active_organization(connection, user_id, personal_org)
     assert shape_exists(connection, shape_id)
     with dep_override_factory(user_id):
@@ -233,35 +157,36 @@ def test_read_shape_diff_user_same_org(connection, dep_override_factory):
     """Read a shape from user in same org"""
 
     user_id = 1
-    shape_id = "8073a55a-98d4-43a7-a1fa-eefab3980f7a"  # Owned by 4
+    shape_id = UUID("8073a55a-98d4-43a7-a1fa-eefab3980f7a")  # Owned by 4
     assert shape_exists(connection, shape_id)
     with dep_override_factory(user_id):
         response = client.get(
             f"/geofencer/shapes/{shape_id}",
         )
         body = response.json()
-        assert body.get("uuid") == shape_id
+        assert UUID(body.get("uuid")) == shape_id
 
 
 def test_read_shape_diff_org(connection, dep_override_factory):
     """Cannot read a shape from a different org."""
     user_id = 1
-    shape_id = "1f053556-c21b-4420-a188-09fc6931eb6f"  # Owned by 3 - diff org
+    # Owned by 3 - diff org
+    shape_id = UUID("1f053556-c21b-4420-a188-09fc6931eb6f")
     assert shape_exists(connection, shape_id)
     with dep_override_factory(user_id):
         response = client.get(f"/geofencer/shapes/{shape_id}", headers=headers)
         body = response.json()
         assert body == None
 
-
 # TODO: What to return if No UUID
-# TODO: What to do if inalid UUID
+# TODO: What to do if invalid UUID
 
 
 def test_read_shape_doesnt_exist(connection, dep_override_factory):
     """Cannot read a shape that doesn't exist."""
     user_id = 1
-    shape_id = "59075662-68d4-408b-afe3-8a0b7af08bee"  # UUID doesn't exist
+    # UUID doesn't exist
+    shape_id = UUID("59075662-68d4-408b-afe3-8a0b7af08bee")
 
     # check that the uuid doesn't exist
     assert not shape_exists(connection, shape_id)
