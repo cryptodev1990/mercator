a941162a0d05cef1bf0b61766d61778821403e7c
cryptodev1990 <raylane19990105@gmail.com>
2022-09-27 19:42:07 +0200

    Add usage query

--- analysis/dubersql/count_usage.sql
+++ analysis/dubersql/count_usage.sql
@@ -12,7 +12,7 @@ GROUP BY 1
 ORDER BY 2 DESC
 ;
 
--- Shapes / emails by organization
+-- Shapes per user
 SELECT email
 , o.name
 , o.id AS organization_uuid
@@ -27,5 +27,22 @@ ON u.id = om.user_id
 WHERE 1=1
   AND om.active
 GROUP BY 1, 2, 3
+;
+
+-- Shape count by org
+SELECT o.name
+, o.id AS organization_uuid
+, COUNT(DISTINCT s.uuid) AS num
+FROM shapes s
+JOIN organizations o
+ON s.organization_id = o.id
+JOIN organization_members om
+ON o.id = om.organization_id
+JOIN users u
+ON u.id = om.user_id
+WHERE 1=1
+  AND om.active
+GROUP BY 1, 2
+ORDER BY 3 DESC
 ;
 
