2b11a097d7df2af858bd3e243f2ddebb80228de3
cryptodev1990 <raylane19990105@gmail.com>
2022-10-03 21:12:30 -0700

    Return to uvicorn (#368)

--- geox-api/start.sh
+++ geox-api/start.sh
@@ -47,7 +47,4 @@ then
     RELOAD_OPT="--reload"
 fi;
 
-# TODO get hypercorn working, this requires dev certificates
-# echo 'NOTE: Running with development certificates'
-# exec hypercorn --certfile cert.pem --keyfile key.pem $RELOAD_OPT --bind "$APP_HOST":"$APP_PORT" --log-level "$APP_LOG_LEVEL" "$APP_MODULE"
-exec hypercorn $RELOAD_OPT --bind "$APP_HOST":"$APP_PORT" --log-level "$APP_LOG_LEVEL" "$APP_MODULE"
+exec uvicorn $RELOAD_OPT --host "$APP_HOST" --port "$APP_PORT" --log-level "$APP_LOG_LEVEL" "$APP_MODULE"
