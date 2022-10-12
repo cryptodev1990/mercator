38321d5adffe88c05b2ee3aace66e2b71b4fb183
cryptodev1990 <raylane19990105@gmail.com>
2022-10-12 11:43:19 -0700

    Remove bad code in test (#417)
    Remove debug code

--- geox-api/test/routes/test_namespaces.py
+++ geox-api/test/routes/test_namespaces.py
@@ -4,21 +4,22 @@
 # Create users
 
 
-def setup_data(conn: Connection):
-    org = create_organization("Example.com")
-    users = [
-        {"name": "Alice", "email": "alice@example.com"}
-        {"name": "Bob", "email": "bob@example.com"}
-    ]
-    for user in users:
-        create_user(user)
-        add_user_to_org()
+# def setup_data(conn: Connection):
+#     org = create_organization("Example.com")
+#     users = [
+#         {"name": "Alice", "email": "alice@example.com"}
+#         {"name": "Bob", "email": "bob@example.com"}
+#     ]
+#     for user in users:
+#         create_user(user)
+#         add_user_to_org()
 # Setup database
 # - Organization 1 - example.com
 #   - Alice
 #   - Bob
 # - Organization 2 - example.net
 #   - Carol
+
 
 def test_namespace():
     """Create a new namespace."""
