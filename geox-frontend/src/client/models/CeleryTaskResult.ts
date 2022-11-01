04f0cc5d550dcd4660d2db3781005e292026e68f
cryptodev1990 <raylane19990105@gmail.com>
2022-11-01 23:27:10 +0500

    add type to task_result of  CeleryTaskResult type

--- geox-frontend/src/client/models/CeleryTaskResult.ts
+++ geox-frontend/src/client/models/CeleryTaskResult.ts
@@ -1,3 +1,5 @@
+import { ApiResult } from "../core/ApiResult";
+import { ApiError } from "../core/ApiError";
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
@@ -6,17 +8,16 @@
  * Result of a Celery task.
  */
 export type CeleryTaskResult = {
-    /**
-     * Task id.
-     */
-    task_id: string;
-    /**
-     * Task status.
-     */
-    task_status: string;
-    /**
-     * Task results.
-     */
-    task_result: any;
+  /**
+   * Task id.
+   */
+  task_id: string;
+  /**
+   * Task status.
+   */
+  task_status: string;
+  /**
+   * Task results.
+   */
+  task_result: ApiResult | ApiError;
 };
-
