c721b04f5858bdd7c8f4c27922471a8f760d01f4
cryptodev1990 <raylane19990105@gmail.com>
2022-11-16 19:23:50 +0500

    add header on dropdown

--- geox-frontend/src/features/geofence-map/components/sidebar/namespace-menu.tsx
+++ geox-frontend/src/features/geofence-map/components/sidebar/namespace-menu.tsx
@@ -20,7 +20,10 @@ const NamespaceMenu = ({
         className="absolute w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
         style={{ top: yPos - 15, left: xPos - 15 }}
       >
-        <ul className="py-1 text-sm text-gray-700 dark:text-gray-200 ">
+        <div className="py-1 text-gray-700 dark:text-gray-200 text-center font-medium">
+          Move Shape
+        </div>
+        <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
           {namespaces
             .filter(
               (namespace) =>
@@ -28,7 +31,7 @@ const NamespaceMenu = ({
             )
             .map((namespace, i) => (
               <li
-                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white whitespace-nowrap"
+                className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white whitespace-nowrap cursor-pointer"
                 key={namespace.id}
                 onClick={() =>
                   partialUpdateShape({
