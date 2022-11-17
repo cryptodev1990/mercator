c1af7c5d7ccb26f3a21c08ee8dca467b62b55526
cryptodev1990 <raylane19990105@gmail.com>
2022-11-18 02:16:04 +0500

    add breadcrumb

--- geox-frontend/src/features/geofence-map/components/sidebar/index.tsx
+++ geox-frontend/src/features/geofence-map/components/sidebar/index.tsx
@@ -7,14 +7,44 @@ import { GeofencerNavbar } from "../navbar";
 import { Footer } from "./footer";
 
 const BackToPaginatorButton = () => {
-  const { setShapeForPropertyEdit } = useShapes();
+  const { shapeForPropertyEdit, setShapeForPropertyEdit } = useShapes();
   return (
-    <button
-      className="text-white rounded hover:text-red-400 bg-opacity-0 border-none capitalize text-sm"
-      onClick={() => setShapeForPropertyEdit(null)}
-    >
-      {"<< Back"}
-    </button>
+    <nav className="flex" aria-label="Breadcrumb">
+      <ol className="inline-flex items-center space-x-1 md:space-x-3">
+        <li className="inline-flex items-center">
+          <a
+            href="#"
+            className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+          >
+            <button
+              className="text-white rounded hover:text-red-400 bg-opacity-0 border-none capitalize text-sm"
+              onClick={() => setShapeForPropertyEdit(null)}
+            >
+              {"Home"}
+            </button>
+          </a>
+        </li>
+        <li aria-current="page">
+          <div className="flex items-center">
+            <svg
+              className="w-6 h-6 text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+              {shapeForPropertyEdit?.name}
+            </span>
+          </div>
+        </li>
+      </ol>
+    </nav>
   );
 };
 
