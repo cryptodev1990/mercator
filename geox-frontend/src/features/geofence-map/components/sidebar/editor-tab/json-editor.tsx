e058a2f85d49b2b623dbfc3cc597cdb089fd0cca
cryptodev1990 <raylane19990105@gmail.com>
2022-11-18 02:44:04 +0500

    allow null in shape meta data editor

--- geox-frontend/src/features/geofence-map/components/sidebar/editor-tab/json-editor.tsx
+++ geox-frontend/src/features/geofence-map/components/sidebar/editor-tab/json-editor.tsx
@@ -81,9 +81,7 @@ export default function JsonEditor({
               <input
                 placeholder="value"
                 type="string"
-                {...register(`properties.${index}.value` as const, {
-                  required: true,
-                })}
+                {...register(`properties.${index}.value` as const)}
                 className={
                   `focus:absolute text-ellipsis col-span-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500${
                     errors?.properties?.[index]?.value ? "error" : ""
