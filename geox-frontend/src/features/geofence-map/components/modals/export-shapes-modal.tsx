a8e213f43edb55b0ec9e3e4ff1df9f93d2f72507
cryptodev1990 <raylane19990105@gmail.com>
2022-12-15 19:10:55 +0500

    add file format selection logic

--- geox-frontend/src/features/geofence-map/components/modals/export-shapes-modal.tsx
+++ geox-frontend/src/features/geofence-map/components/modals/export-shapes-modal.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { DownloadIcon } from "../../../../common/components/icons";
+import { CancelIcon, DownloadIcon } from "../../../../common/components/icons";
 import { useGetAllShapes } from "../../hooks/use-openapi-hooks";
 import { useShapes } from "../../hooks/use-shapes";
 import { useUiModals } from "../../hooks/use-ui-modals";
 import { UIModalEnum } from "../../types";
 import { geoShapesToFeatureCollection } from "../../utils";
 import { ModalCard } from "./modal-card";
+import { topology } from "topojson-server";
+import { BiCaretDown } from "react-icons/bi";
+import clsx from "clsx";
 
 export const ExportShapesModal = () => {
   const { modal, closeModal } = useUiModals();
@@ -14,6 +17,11 @@ export const ExportShapesModal = () => {
   const offset = useState(0)[0];
   const { data } = useGetAllShapes(limit, offset);
   const [exportable, setExportable] = useState<any>();
+  const [selectedFormat, setSelectedFormat] = useState("Geojson");
+
+  const handleOptionSelect = (option: string) => {
+    setSelectedFormat(option);
+  };
 
   useEffect(() => {
     if (data) {
@@ -30,15 +38,31 @@ export const ExportShapesModal = () => {
         if (!data) {
           return;
         }
-        const dataStr =
-            "data:text/json;charset=utf-8," +
-            encodeURIComponent(JSON.stringify(exportable)),
-          downloadAnchorNode = document.createElement("a");
-        downloadAnchorNode.setAttribute("href", dataStr);
-        downloadAnchorNode.setAttribute("download", "data.json");
-        document.body.appendChild(downloadAnchorNode); // required for firefox
-        downloadAnchorNode.click();
-        downloadAnchorNode.remove();
+        if (selectedFormat === "Geojson") {
+          const dataStr =
+              "data:text/json;charset=utf-8," +
+              encodeURIComponent(JSON.stringify(exportable)),
+            downloadAnchorNode = document.createElement("a");
+          downloadAnchorNode.setAttribute("href", dataStr);
+          downloadAnchorNode.setAttribute("download", "data.geojson");
+          document.body.appendChild(downloadAnchorNode); // required for firefox
+          downloadAnchorNode.click();
+          downloadAnchorNode.remove();
+        }
+
+        if (selectedFormat === "Topojson") {
+          const exportData = topology({ exportable });
+          const dataStr =
+              "data:text/json;charset=utf-8," +
+              encodeURIComponent(JSON.stringify(exportData)),
+            downloadAnchorNode = document.createElement("a");
+          downloadAnchorNode.setAttribute("href", dataStr);
+          downloadAnchorNode.setAttribute("download", "data.topojson");
+          document.body.appendChild(downloadAnchorNode); // required for firefox
+          downloadAnchorNode.click();
+          downloadAnchorNode.remove();
+        }
+
         closeModal();
       }}
       icon={
@@ -60,7 +84,71 @@ export const ExportShapesModal = () => {
           </a>{" "}
           for developer API access.
         </p>
+        <FileFormatSelection
+          options={["Geojson", "Topojson"]}
+          handleOptionSelect={handleOptionSelect}
+          initialOption="Geojson"
+        />
       </div>
     </ModalCard>
+  );
+};
+
+// dropdown of the current namespaces
+const FileFormatSelection = ({
+  options,
+  handleOptionSelect,
+  initialOption,
+}: any) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(initialOption);
+
+  const handleDropdownClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className="flex flex-row items-center">
+      <div className="flex flex-row items-center">
+        <p className="text-black text-sm mr-2">Select Format</p>
+        <div className="flex flex-row items-center">
+          <div
+            className="flex flex-row items-center justify-center bg-white rounded w-full h-8 m-2 p-2"
+            onClick={handleDropdownClick}
+          >
+            <BiCaretDown className="fill-black" />
+            <p
+              className={clsx({
+                ["text-black text-sm"]: true,
+                ["opacity-50"]: selectedOption,
+              })}
+            >
+              {selectedOption || "Select Format"}{" "}
+            </p>
+          </div>
+          {isDropdownOpen && (
+            <div className="absolute z-10 bg-white rounded-md shadow-md">
+              <div className="text-black">
+                <CancelIcon
+                  className="float-right m-1 hover:cursor-pointer"
+                  onClick={() => setIsDropdownOpen(false)}
+                />
+              </div>
+              {options.map((option: string) => (
+                <div
+                  className="flex flex-row items-center justify-start w-full h-8 rounded-md cursor-pointer hover:bg-gray-200 p-2"
+                  onClick={() => {
+                    setSelectedOption(option);
+                    handleOptionSelect(option);
+                  }}
+                >
+                  <p className="text-black text-sm">{option}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
