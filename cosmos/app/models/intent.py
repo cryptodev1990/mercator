f24f1efc8995c9f464f42b23974b0a235e418f96
cryptodev1990 <raylane19990105@gmail.com>
2022-12-20 12:43:15 -0800

    Fix voyager test (#782)
    bugfix: fix voyager test

--- cosmos/app/models/intent.py
+++ cosmos/app/models/intent.py
@@ -71,7 +71,7 @@ def funcs_for_openai(intent_name: str, module) -> str:
     # assert that the result matches a regex for a python function signature
     assert re.match(r'def \w+\(.*\)', f), f'Function signature {f} does not match regex'
     # We strip out the database connection info
-    f = re.sub(r', conn: psycopg.AsyncConnection', '', f)
+    f = re.sub(r', conn: .*?AsyncConnection', '', f)
     return f
 
 def _each_intent_has_a_parser(intents_dict: Dict[str, Intent]):
