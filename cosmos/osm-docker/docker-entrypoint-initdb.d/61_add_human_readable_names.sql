6f3a88464f6a11ef63277c15c4e49cbf3de48280
cryptodev1990 <raylane19990105@gmail.com>
2022-12-30 13:24:12 -0800

    Add human-readable categories (#805)

---
+++ cosmos/osm-docker/docker-entrypoint-initdb.d/61_add_human_readable_names.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+-- copy CSV of OpenAI-generated human readable names into database
+CREATE TEMPORARY TABLE osm_human_readable_categories (
+  slug VARCHAR,
+  human_readable VARCHAR
+);
+
+CREATE INDEX ON osm_human_readable_categories(slug);
+-- TODO how do we automate this copy?
+SELECT 'NOTE copy the file cleaned_categories.dsv before running this script or it will fail' AS message;
+\copy osm_human_readable_categories FROM '/tmp/cleaned_categories.dsv' DELIMITER '|' CSV HEADER;
+
+CREATE TEMPORARY TABLE category_membership_swap AS
+  SELECT osm_id
+  , category
+  , human_readable
+  FROM category_membership
+  JOIN osm_human_readable_categories
+  ON slug = category
+;
+
+-- promote
+DROP TABLE category_membership;
+CREATE TABLE category_membership AS
+SELECT *
+FROM category_membership_swap;
+
+END;
