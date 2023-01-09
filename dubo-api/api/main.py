f964736f0dec3cae8a1f66d873600c8f4c1779ba
cryptodev1990 <raylane19990105@gmail.com>
2023-01-08 17:17:56 -0800

    [dubo-api] Update backend for new client (#828)

--- dubo-api/api/main.py
+++ dubo-api/api/main.py
@@ -1,7 +1,7 @@
+from typing import List, Optional
 import os
-import re
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 import openai
 from pydantic import BaseModel
 import sqlparse
@@ -26,41 +26,66 @@ class QueryResponse(BaseModel):
     query_text: str
 
 
-@app.get("/v1/dubo/query", 
+@app.get("/v1/dubo/query",
     response_model=QueryResponse,
     responses={
         422: {"description": "Invalid schema"},
         429: {"description": "Too many requests"},
         500: {"description": "No suitable response for the given query."},
         503: {"description": "Service unavailable"}
-    }
+    },
+    tags=["dubo"],
+    summary="Convert text to SQL",
 )
 def read_query(
-    schema: str,
-    query: str
-):
-    parsed = sqlparse.parse(schema)[0]
-    # Really rough check to make sure the input is a valid SQL table schema
-    assert parsed.tokens[0].value.upper() == "CREATE", "Invalid schema"
+    user_query: str = Query(default=None, description="The question to answer"),
+    schemas: List[str] = Query(default=[], description="The table schema(s) to use"),
+    descriptions: List[str] | None = Query(default=[], description="The table description(s) to use")
+): 
+    if not user_query:
+        raise HTTPException(status_code=422, detail="No query provided")
+    if not schemas:
+        raise HTTPException(status_code=422, detail="No schemas provided")
+    try:
+        descriptions = descriptions or []
+        parsed = sqlparse.parse(schemas[0])[0]
+        # Really rough check to make sure the input is a valid SQL table schema
+        assert parsed.tokens[0].value.upper() == "CREATE", "Invalid schema"
+        for s_to_check in [user_query, *schemas, *descriptions]:
+            if not s_to_check:
+                continue
+            assert '```' not in s_to_check, "Reserved tokens in query"
+            assert '"""' not in s_to_check, "Reserved tokens in query"
+    except AssertionError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+
+    schema = '\n'.join(schemas)
+
+    print(schema)
+
     prompt = f'''
     Convert text to SQL.
-    
-    You have a table of input with the following schema:
-    
+
+    You have the following DDLs:
+
+    ```
     {schema}
-    
+    ```
+
     Write the SQL that answers the following question:
-    
-    """{query}"""
-    
-    Be specific.
+
+    """{user_query}"""
+
+    Respond with only one concise SQL statement.
     '''
+
+    print(prompt)
 
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         temperature=0.7,
-        max_tokens=500,
+        max_tokens=1000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
@@ -70,5 +95,4 @@ def read_query(
         return {
             "query_text": response.choices[0].text.strip() # type: ignore
         }
-    else:
-        raise Exception("No suitable response for the given query.")
+    raise HTTPException(status_code=400, detail="No suitable response for the given query.")
--- dubo-api/test/test_main.py
+++ dubo-api/test/test_main.py
@@ -1,3 +1,4 @@
+import sqlite3
 import pandas as pd
 import dubo
 from fastapi.testclient import TestClient
@@ -13,12 +14,31 @@ def test_read_root():
 
 
 def test_read_query():
-    response = client.get("/v1/dubo/query", params={"schema": "create TABLE tbl (bar INT);", "query": "Sum the values in bar"})
+    response = client.get("/v1/dubo/query", params={"schemas": ["create TABLE tbl (bar INT);"], "user_query": "What are the total values in bar?"})
     assert response.status_code == 200
     assert response.json()["query_text"] == "SELECT SUM(bar) FROM tbl;"
 
 
 def test_read_query_with_dubo():
-    response = client.get("/v1/dubo/query?query=Sum+the+values+in+bar&schema=CREATE+TABLE+%22tbl%22+%28%0A%22bar%22+INTEGER%0A%29")
+    response = client.get("/v1/dubo/query?user_query=What%20are%20the%20coolest%20bars%20in%20Des%20Moines%3F&schemas=CREATE%20TABLE%20bars%20%28id%20INTEGER%2C%20coolest%20BOOLEAN%29%3B&schemas=CREATE%20TABLE%20des_moines_bars%20%28id%20INTEGER%29%3B")
     assert response.status_code == 200
-    assert response.json()["query_text"] == "SELECT SUM(bar) FROM tbl;"
+    query = response.json()["query_text"].replace('\n', ' ')
+
+    # Create an in-memory SQLite database
+    conn = sqlite3.connect(":memory:")
+    c = conn.cursor()
+
+    # Create the tables
+    c.execute("CREATE TABLE bars (id INTEGER, coolest BOOLEAN);")
+    c.execute("CREATE TABLE des_moines_bars (id INTEGER);")
+
+    # Insert some data
+    c.execute("INSERT INTO bars VALUES (1, True);")
+    c.execute("INSERT INTO bars VALUES (2, False);")
+    c.execute("INSERT INTO des_moines_bars VALUES (1);")
+
+    # Execute the query
+    c.execute(query)
+
+    # Check the results
+    assert c.fetchall() == [(1,)]
