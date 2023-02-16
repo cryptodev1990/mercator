20d83748c693d95f7e419b032e1e51d7cb47173b
cryptodev1990 <raylane19990105@gmail.com>
2023-02-16 14:12:54 -0800

    [dubo-frontend] [bug] Fix text tracking on safari (#1025)

--- dubo-frontend/components/tracked-text.tsx
+++ dubo-frontend/components/tracked-text.tsx
@@ -1,8 +1,8 @@
 export const TrackedText = ({ text }: { text: string }) => {
   return (
     <div className="relative tracking-tighter">
-      <h1 className="text-2xl font-black font-outline-4 z-0">{text}</h1>
-      <span className="text-2xl absolute top-0 left-0 z-30 text-slate-900">
+      <h1 className="text-2xl font-outline-4 z-0 tracking-tighter">{text}</h1>
+      <span className="text-2xl absolute top-0 left-0 z-30 text-slate-900 tracking-tighter">
         {text}
       </span>
     </div>
